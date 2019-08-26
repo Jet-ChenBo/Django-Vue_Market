628c40ce762c42a4accddecdf831a40c0979e6be
Jet-ChenBo <862354034@qq.com>
2019-08-26 22:22:59 +0800

    配置使用mysql数据库

--- dvMarket/settings.py
+++ dvMarket/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'dvMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dvMarket',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': {'init_command': 'SET storage_engine=INNODB;'}  # 第三方登录需要
     }
 }
 
